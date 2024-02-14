aa3dbf110b02bd40f92e956254e58a13a7fc1367
Lorenzo Cimini <lorenzocimini@hotmail.it>
2024-02-14 19:07:12 +0100

    reciprocal3

--- benches/benchmarks/fastdiv.rs
+++ benches/benchmarks/fastdiv.rs
@@ -106,6 +106,54 @@ fn get_multiplication_parameters2(divisor: u16) -> Reciprocal2 {
     }
 }
 
+struct Reciprocal3 {
+    a: u64,
+    magic: u8,
+}
+
+/// This function returns the parameters a, b and m, that is the value used to get the same result
+/// we would get by dividing any number by the divisor if a multiplication is performed.
+fn get_multiplication_parameters3(divisor: u16) -> Reciprocal3 {
+    let m = divisor.ilog2();
+
+    match divisor.is_power_of_two() {
+        // If the divisor (d) is power of two, then:
+        // a := 2^n - 1 = 2^64 - 1;
+        // b := 2^n - 1 = 2^64 - 1;
+        // m := log_{2}(d)
+        // finally:
+        // floor(x / d)  = floor((ax + b)) / 2^n) / 2^m
+        true => Reciprocal3 {
+            a: u64::MAX,
+            magic: ((m as u8) << 1) + 1_u8,
+        },
+        // else:
+        // t := floor(2^m+n / d);
+        // r := (d * (t - 1)) - 2^m+n;
+        // if r <= 2^m then:
+        // a := t + 1; -> rounding up the reciprocal
+        // b := 0;
+        // else:
+        // a := t; -> rounding down the reciprocal
+        // b := t;
+        false => {
+            let t = ((1u128 << m + 64) / divisor as u128) as u64;
+            let r = (divisor as u128 * (t as u128 + 1) - (1u128 << (m + 64))) as u64;
+
+            match r <= (1u64 << m) {
+                true => Reciprocal3 {
+                    a: t + 1,
+                    magic: (m as u8) << 1,
+                },
+                false => Reciprocal3 {
+                    a: t,
+                    magic: ((m as u8) << 1) + 1_u8,
+                },
+            }
+        }
+    }
+}
+
 pub fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("div");
     let dividend = 0xdeadbeefdeadf00d_u64;
@@ -144,6 +192,19 @@ pub fn criterion_benchmark(c: &mut Criterion) {
                 (black_box(reciprocal.a) as u128 * (black_box(dividend) as u128 + 1) >> 64) as u64
                     >> black_box(reciprocal.shift)
             })
+        });
+    });
+
+    let reciprocal3 = get_multiplication_parameters3(4242);
+
+    group.bench_function("with magic", |b| {
+        b.iter(|| {
+            black_box(
+                (black_box(reciprocal3.a) as u128
+                    * (black_box(dividend) as u128 + black_box(reciprocal3.magic & 1_u8) as u128)
+                    >> 64) as u64
+                    >> black_box(reciprocal3.magic >> 1),
+            )
         });
     });
 }
