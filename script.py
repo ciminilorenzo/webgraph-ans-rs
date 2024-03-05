d2300e851d3f9311a937544ed9ab4b04da20fdc6
Lorenzo Cimini <lorenzocimini@hotmail.it>
2024-03-05 17:14:11 +0100

    speed time

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import sys
 import csv
+import re
 
 # Graphs are: cnr-2000 and in-2004
 ans_graphs = ['cnr-2000', 'in-2004']
@@ -19,9 +20,13 @@ if not os.path.isdir(graphs_dir):
 
 # Build bvcomp
 subprocess.run(["cargo", "build", "--release", "--bin", "bvcomp"])
+# Build bvtest
+subprocess.run(["cargo", "build", "--release", "--bin", "bvtest"])
 
 ans_sizes = []
 ans_phases_sizes = []
+random_access_speed = []
+sequential_access_speed = []
 
 for graph in ans_graphs:
     print(f"Starting compression of {graph}")
@@ -32,12 +37,31 @@ for graph in ans_graphs:
     phases_size = os.path.getsize(f"{compressed_graphs_dir}{graph}.phases")
     ans_phases_sizes.append(phases_size)
 
+    print(f"Starting random/sequential speed test of {graph}")
+
+    timing = subprocess.run([
+        "./target/release/bvtest",
+        f"{compressed_graphs_dir}{graph}",
+    ],
+        stderr=subprocess.PIPE,
+    )
+
+    # Define regular expression pattern
+    pattern = r'INFO - Elapsed: (\d+\w{1,2}) \['
+
+    # Find all matches
+    matches = re.findall(pattern, f"{timing}")
+
+    random = matches[0]
+    sequential = matches[1]
+    random_access_speed.append(random)
+    sequential_access_speed.append(sequential)
 
 with open('results.csv', 'w', encoding='UTF8', newline='') as f:
     # create the csv writer
     writer = csv.writer(f)
     # write the header
-    writer.writerow(['name', 'BVGraph', 'ANSBVGraph', 'improvement', 'bit/link', 'phases'])
+    writer.writerow(['name', 'BVGraph', 'ANSBVGraph', 'improvement', 'bit/link', 'phases', 'random', 'sequential'])
 
     for index in range(len(ans_graphs)):
         bit_link = "{:.3f}".format((ans_sizes[index] * 8) / arcs[index])
@@ -48,7 +72,9 @@ with open('results.csv', 'w', encoding='UTF8', newline='') as f:
             "{} B".format(ans_sizes[index]),
             improvement,
             bit_link,
-            "{} B".format(ans_phases_sizes[index])
+            "{} B".format(ans_phases_sizes[index]),
+            random_access_speed[index],
+            sequential_access_speed[index],
         ]
 
         # write the data
--- src/bin/bvtest.rs
+++ src/bin/bvtest.rs
@@ -5,6 +5,7 @@ use anyhow::Result;
 use clap::Parser;
 use dsi_progress_logger::*;
 use folded_streaming_rans::bvgraph::random_access::ANSBVGraph;
+use lender::for_;
 use rand::rngs::SmallRng;
 use rand::Rng;
 use rand::SeedableRng;
@@ -15,9 +16,6 @@ use webgraph::prelude::*;
 struct Args {
     /// The basename of the graph.
     basename: String,
-    /// The number of nodes to test.
-    #[clap(short, long, default_value_t = 1000)]
-    n: usize,
 }
 
 pub fn main() -> Result<()> {
@@ -33,18 +31,31 @@ pub fn main() -> Result<()> {
     let mut pl = ProgressLogger::default();
     let mut rng = SmallRng::seed_from_u64(0);
 
-    pl.start("Testing successors...");
-    for _ in 0..args.n {
+    let random_nodes = (0..graph.num_nodes())
+        .into_iter()
+        .map(|_| rng.gen_range(0..graph.num_nodes()))
+        .collect::<Vec<_>>();
+
+    pl.start("Testing random node's successors...");
+    for random_mode in random_nodes.iter() {
         let mut d = 0;
-        graph
-            .successors(rng.gen_range(0..graph.num_nodes()))
-            .for_each(|x| {
-                d += 1;
-                black_box(x);
-            });
+        graph.successors(*random_mode).for_each(|x| {
+            d += 1;
+            black_box(x);
+        });
         pl.update_with_count(d);
     }
     pl.done();
 
+    pl.start("Testing successor of sequential nodes");
+    for_![ (_, s) in graph.iter() {
+        let mut d = 0;
+        s.for_each(|x| {
+            d += 1;
+            black_box(x);
+        });
+        pl.update_with_count(d);
+    }];
+    pl.done();
     Ok(())
 }
