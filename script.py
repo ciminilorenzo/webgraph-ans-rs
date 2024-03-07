33e66e55d923225444236b659e950034436dd669
Lorenzo Cimini <lorenzocimini@hotmail.it>
2024-03-07 17:55:50 +0100

    improved script

--- script.py
+++ script.py
@@ -36,23 +36,17 @@ random_access_speed = []
 sequential_access_speed = []
 
 for graph in ans_graphs:
-    number_of_arcs = int(subprocess.run([
-        "grep", "-w", "arcs", f"{graphs_dir}{graph}/{graph}.properties", "|", "cut", "-d'='", "-f2"
-    ]).stdout.decode('utf-8'))
-
+    command = f"grep -w arcs {graphs_dir}{graph}/{graph}.properties | cut -d'=' -f2"
+    number_of_arcs = int(subprocess.run(command, capture_output=True, shell=True).stdout.decode('utf-8'))
     arcs.append(number_of_arcs)
 
-    bit_per_link = subprocess.run([
-        "grep", "bitsperlink", f"{graphs_dir}{graph}/{graph}.properties", "|", "cut", "-d'='", "-f2"
-    ])
+    command = f"grep -w bitsperlink {graphs_dir}{graph}/{graph}.properties | cut -d'=' -f2"
+    bit_per_link = float(subprocess.run(command, capture_output=True, shell=True).stdout.decode('utf-8').strip("/n"))
+    bv_graphs_size.append(int((bit_per_link * number_of_arcs) / 8))
 
-    bv_graphs_size.append((int(bit_per_link.stdout.decode('utf-8')) * number_of_arcs) / 8)
-
-    bit_per_link_hc = subprocess.run([
-        "grep", "bitsperlink", f"{graphs_dir}{graph}/{graph}-hc.properties", "|", "cut", "-d'='", "-f2"
-    ])
-
-    bv_hc_graphs_size.append((int(bit_per_link_hc.stdout.decode('utf-8')) * number_of_arcs) / 8)
+    command = f"grep -w bitsperlink {graphs_dir}{graph}/{graph}-hc.properties | cut -d'=' -f2"
+    hc_bit_per_link = float(subprocess.run(command, capture_output=True, shell=True).stdout.decode('utf-8').strip("/n"))
+    bv_hc_graphs_size.append(int((hc_bit_per_link * number_of_arcs) / 8))
 
     print(f"Starting standard compression of {graph}")
     subprocess.run([
@@ -68,7 +62,7 @@ for graph in ans_graphs:
         f"{compressed_graphs_dir}{graph}-hc",
         "-w", f"{high_compressed_params.get('w')}",
         "-c", f"{high_compressed_params.get('c')}"
-        ])
+    ])
 
     ans_sizes.append(os.path.getsize(f"{compressed_graphs_dir}{graph}.ans"))
     ans_hc_sizes.append(os.path.getsize(f"{compressed_graphs_dir}{graph}-hc.ans"))
@@ -97,12 +91,12 @@ with open('results.csv', 'w', encoding='UTF8', newline='') as f:
     # write the header
     writer.writerow([
         'name',
-        '.graph',
-        '.ans',
+        'BVGraph',
+        'ANSBVGraph',
         'bit/link',
-        'hc.graph',
-        'hc.ans',
-        'hc bit/link',
+        'HC-BVGraph',
+        'HC-ANSBVGraph',
+        'HC-bit/link',
         'phases',
         'random speed',
         'sequential speed'
@@ -111,34 +105,30 @@ with open('results.csv', 'w', encoding='UTF8', newline='') as f:
     for index in range(len(ans_graphs)):
         # bit/link .ans
         bit_link = "{:.3f}".format((ans_sizes[index] * 8) / arcs[index])
-        # How much we save in space w.r.t the .graph
-        occupation = "-{:.0f}%".format((bv_graphs_size[index] - ans_sizes[index]) / bv_graphs_size[index] * 100)
+        # How much we store w.r.t the .graph (if negative, we are using less space)
+        occupation = "{:.0f}%".format(-(((bv_graphs_size[index] - ans_sizes[index]) / bv_graphs_size[index]) * 100))
         # bit/link -hc.ans
         hc_bit_link = "{:.3f}".format((ans_hc_sizes[index] * 8) / arcs[index])
         # How much we save in space w.r.t the -hc.graph
-        occupation_hc = "-{:.0f}%".format((bv_hc_graphs_size[index] - ans_hc_sizes[index]) / bv_hc_graphs_size[index] * 100)
-        # bit/link .graph
-        bv_graphs_bit_link = "{:.2f}".format(ans_sizes[index] * 8 / arcs[index])
-        # bit/link -hc.graph
-        bv_hc_graphs_bit_link = "{:.2f}".format(ans_hc_sizes[index] * 8 / arcs[index])
+        occupation_hc = "{:.0f}%".format(-(((bv_hc_graphs_size[index] - ans_hc_sizes[index]) / bv_hc_graphs_size[index]) * 100))
         # .obl size
         obl_size = os.path.getsize(f"{graphs_dir}{ans_graphs[index]}/{ans_graphs[index]}.obl")
         # How much we spend w.r.t the .obl
-        occupation_phases = "{:.0f}%".format((-(obl_size - ans_phases_sizes[index]) / obl_size * 100))
+        occupation_phases = "{:.0f}%".format(-(((obl_size - ans_phases_sizes[index]) / obl_size) * 100))
 
         data = [
             ans_graphs[index],
             "{} B".format(bv_graphs_size[index]),
             "{} B".format(ans_sizes[index]),
-            "{}({})".format(bit_link, occupation),
+            "{} ({})".format(bit_link, occupation),
             "{} B".format(bv_hc_graphs_size[index]),
             "{} B".format(ans_hc_sizes[index]),
-            "{}({})".format(hc_bit_link, occupation_hc),
-            "{}({}) B".format(ans_phases_sizes[index], occupation_phases),
+            "{} ({})".format(hc_bit_link, occupation_hc),
+            "{} B({})".format(ans_phases_sizes[index], occupation_phases),
             random_access_speed[index],
             sequential_access_speed[index],
         ]
 
         writer.writerow(data)
 
-print("Saving results in ./results.csv")
+    print("Saving results in ./results.csv")
